fac1b53be8ce73354818199393f76b2c79008b20
Dalton Hall <dhall.07113@gmail.com>
2022-02-20 08:28:41 -0600

    first commit

---
+++ ManipulatingDom.js
@@ -0,0 +1,67 @@
+ //OBJECTIVE 1
+addEventListener("DOMContentLoaded", button);
+function button() {
+    let button = document.createElement("button");
+    let buttonText = document.createTextNode("Click Me!");
+    button.appendChild(buttonText);
+    document.body.appendChild(button);
+    addEventListener("click", click);
+    function click() {
+        alert("nice message.")
+    }
+}
+
+
+//OBJECTIVE 2 
+let newbutton = document.getElementById("newbutton");
+newbutton.addEventListener("click", click);
+function click() {
+let message = document.getElementById("text-box").value;
+alert(message);
+}
+
+
+//OBJECTIVE 3 
+let div = document.getElementById("div");
+div.addEventListener("mouseenter", function() {
+    div.style.backgroundColor = "red";
+})
+div.addEventListener("mouseleave", function() {
+    div.style.backgroundColor = "black";
+})
+
+
+//OBJECTIVE 4
+let p = document.getElementById("p");
+p.addEventListener("click", function() {
+    let allchar = "0123456789ABCDEF";
+    let randcol = "";
+    for (let i = 0; i < 6; i++) {
+        randcol += allchar[Math.floor(Math.random() * 16)];
+    }
+    p.style.color = "#" + randcol;
+})
+
+
+//OBJECTIVE 5
+let newerbutton = document.getElementById("newerbutton");
+let newdiv = document.getElementById("div");
+newerbutton.addEventListener("click", function() {
+    let span = document.createElement("span");
+    div.appendChild(span);
+    let text = document.createTextNode("Dalton");
+    span.appendChild(text);
+})
+
+//Objective 6
+let friends = ["Brody", "Will", "Madison", "Trent", "Jerry", "Kasyn", "Zach", "Landon", "Peyton", "Cole"];
+let newestbutton = document.getElementById("newestbutton");
+let ul = document.getElementById("ul");
+newestbutton.addEventListener("click", function () {
+    for (let i = 0; i < friends.length; i++) {
+        let li = document.createElement("li");
+        let name = document.createTextNode(friends[i]);
+        li.appendChild(name);
+        ul.appendChild(li);
+    }
+});
---
+++ ManipulatingjQuery/ManipulatingjQuery.js
@@ -0,0 +1,56 @@
+// OBJECTIVE 1
+let $button = $("<button>Click Me!</button>");
+$button.appendTo("body");
+$button.click(function () {
+    alert("Nice Message.")
+});
+
+
+// OBJECTIVE 2
+let $newbutton = $("#newbutton");
+$newbutton.click( function () {
+    let message = $("#text-box").val();
+    alert(message);
+});
+
+
+// OBJECTIVE 3
+let $div = $("#div");
+$div.hover(function() {
+    div.style.backgroundColor = "red";
+}, function() {
+    div.style.backgroundColor = "black";
+});
+
+
+// OBJECTIVE 4
+let $p = $("#p");
+$p.click( function() {
+    let $allchar = "0123456789ABCDEF";
+    let $randcol = "";
+    for (let i = 0; i < 6; i++) {
+        $randcol += $allchar[Math.floor(Math.random() * 16)];
+    }
+    $($p).css("color", "#" + $randcol);
+});
+
+
+// OBJECTIVE 5
+let $newerbutton = $("#newerbutton");
+let $newdiv = $("#newdiv");
+$newerbutton.click( function() {
+    let $span = $("<span>Dalton</span>");
+    $($span).appendTo($newdiv);
+});
+
+
+// OBJECTIVE 6
+let $friends = ["Madison", "Zach", "Brody", "Trent", "Jerry", "Will", "Jake", "Peyton", "Landon", "Randy"];
+let $newestbutton = $("#newestbutton");
+let $ul = $("#ul");
+$($newestbutton).click( function () {
+    for (let i = 0; i < $friends.length; i++) {
+        let $li = $("<li>" + ($friends[i]) + "</li>");
+        $($li).appendTo($ul);
+    }
+});
